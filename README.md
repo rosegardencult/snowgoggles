752d7cb3155eff33fb29ded554ba0df43f25af65
juniper wilde <juniper.r.wilde@gmail.com>
2020-04-18 17:56:52 -0700

    init!

---
+++ README.md
@@ -0,0 +1,1 @@
+# snowgoggles rendering layer
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 rose garden cult All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//===----------------------------------------------------------------------===//
+//
+// Warmth Engine Library root entry point
+//
+//===----------------------------------------------------------------------===//
+
+#![doc(issue_tracker_base_url = "https://github.com/rosegardencult/snowgoggles/issues/")]
